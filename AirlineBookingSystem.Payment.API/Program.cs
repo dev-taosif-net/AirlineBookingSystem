197740c50379d02636f36a32ef217080a3e57cbf
taosif <dev.taosif.net@gmail.com>
2025-07-23 08:39:45 +0600

    Add Payment repository implementation and configure database connection

--- AirlineBookingSystem.Payment.API/Program.cs
+++ AirlineBookingSystem.Payment.API/Program.cs
@@ -1,8 +1,15 @@
+using System.Data;
+using AirlineBookingSystem.Payment.Core.Repositories;
+using AirlineBookingSystem.Payment.Infrastructure.Repositories;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+builder.Services.AddScoped<IDbConnection>(x => new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
---
+++ AirlineBookingSystem.Payment.Infrastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using AirlineBookingSystem.Payment.Core.Repositories;
+using Dapper;
+
+namespace AirlineBookingSystem.Payment.Infrastructure.Repositories;
+
+public class PaymentRepository(IDbConnection dbConnection) : IPaymentRepository
+{
+    public Task ProcessPaymentAsync(Core.Entities.Payment payment)
+    {
+        const string sql = @"INSERT INTO [dbo].[Payments] ([Id], [BookingId], [Amount], [PaymentDate]) VALUES (@Id, @BookingId, @Amount, @PaymentDate);";
+        return dbConnection.ExecuteAsync(sql, payment);
+    }
+
+    public Task RefundPaymentAsync(Guid paymentId)
+    {
+        const string sql = $"DELETE FROM Payments WHERE Id = @Id";
+        return dbConnection.ExecuteAsync(sql, new { Id = paymentId });
+    }
+}
