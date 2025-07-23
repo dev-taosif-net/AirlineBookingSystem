9e0362e76581baa25575fd235cf84fee8d19d289
taosif <dev.taosif.net@gmail.com>
2025-07-23 08:31:29 +0600

    Add Notification repository and configure database connection

--- AirlineBookingSystem.Notification.API/Program.cs
+++ AirlineBookingSystem.Notification.API/Program.cs
@@ -1,8 +1,15 @@
+using System.Data;
+using AirlineBookingSystem.Notification.Core.Repositories;
+using AirlineBookingSystem.Notification.Infrastructure.Repositories;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
+builder.Services.AddScoped<IDbConnection>(x => new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
---
+++ AirlineBookingSystem.Notification.Infrastructure/Repositories/NotificationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using AirlineBookingSystem.Notification.Core.Repositories;
+using Dapper;
+
+namespace AirlineBookingSystem.Notification.Infrastructure.Repositories;
+
+public class NotificationRepository(IDbConnection dbConnection) : INotificationRepository
+{
+    public Task LogNotificationAsync(Core.Entities.Notification notification)
+    {
+        const string sql = @"INSERT INTO [AirlineBookingSystem].[dbo].[Notifications] ([Id], [Recipient], [Message], [Type], [SentAt]) VALUES (@Id, @Recipient, @Message, @Type, @SentAt)";
+        return dbConnection.ExecuteAsync(sql, notification);
+    }
+}
